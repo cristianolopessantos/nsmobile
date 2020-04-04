d82b3c89246007414f5d20d4b1aa2c9b081de5aa
cristianolopessantos <cristianosantos@MacBook-Pro-de-Cristiano.local>
2020-04-04 12:16:42 -0300

    chore(update react native version to 0.61.2)

---
+++ ios/Dummy.swift
@@ -0,0 +1,8 @@
+//
+//  Dummy.swift
+//  netseries_app
+//
+//  Created by Cristiano  Santos on 04/04/20.
+//
+
+import Foundation
---
+++ ios/Pods/Target Support Files/Pods-netseries_app-netseries_appTests/Pods-netseries_app-netseries_appTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_netseries_app_netseries_appTests : NSObject
+@end
+@implementation PodsDummy_Pods_netseries_app_netseries_appTests
+@end
